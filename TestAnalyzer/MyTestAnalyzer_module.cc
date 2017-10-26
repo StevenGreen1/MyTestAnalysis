f2d9eebdac4a0818ba8572d06bad31420caa1069
Steven Green <sg1sg2sg3@hotmail.co.uk>
2017-10-26 12:26:42 +0100

    SG tutorial work.

--- TestAnalyzer/MyTestAnalyzer_module.cc
+++ TestAnalyzer/MyTestAnalyzer_module.cc
@@ -22,6 +22,9 @@
 #include "TFile.h"                                                                                                             
 #include "art/Framework/Services/Optional/TFileService.h"
 
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "canvas/Persistency/Common/FindManyP.h"
+
 namespace test {
   class MyTestAnalyzer;
 }
@@ -47,11 +50,17 @@ public:
   void reconfigure(fhicl::ParameterSet const & p) override;
 
 private:
+    typedef std::vector< art::Ptr<recob::Hit>> HitVector;
+    typedef std::map< art::Ptr<recob::Cluster>, HitVector> ClustersToHits;
 
   // Declare member data here.
   TFile * m_file;
   TTree * m_tree;
-
+  unsigned int nEvent;
+    std::string m_particleLabel;
+    int m_nPFParticles;
+    unsigned int m_nTracks;
+    unsigned int m_nShowers;
 };
 
 
@@ -60,12 +69,18 @@ test::MyTestAnalyzer::MyTestAnalyzer(fhicl::ParameterSet const & p)
   EDAnalyzer(p)  // ,
  // More initializers here.
 {
+    nEvent = 0;
+    m_nPFParticles = -1;
+    m_nTracks = 0;
+    m_nShowers = 0;
+
   //create output tree                                                                                                             
   art::ServiceHandle<art::TFileService> tfs;                                                                                       
   m_file = new TFile("MyTestAnalyzer.root", "RECREATE");                                                                 
   m_tree = tfs->make<TTree>("tree","Tree");
   //add branches
-  
+    m_tree->Branch("nEvent",&nEvent,"nEvent/I");  
+    m_tree->Branch("nPFParticles",&m_nPFParticles,"nPFParticles/I");  
 
   this->reconfigure(p);
 
@@ -83,14 +98,44 @@ test::MyTestAnalyzer::~MyTestAnalyzer()
 void test::MyTestAnalyzer::analyze(art::Event const & e)
 {
   // Implementation of required member function here.
+    art::Handle< std::vector<recob::Hit>
 
-  //your analysis goes here! 
+    art::Handle< std::vector<recob::PFParticle> > theParticles;
+    e.getByLabel(m_particleLabel, theParticles);
+    m_nPFParticles = -1;
+    m_nPFParticles = theParticles->size();
+
+    nEvent++;
+
+    m_nTracks = 0;
+    m_nShowers = 0;
+
+    for (unsigned int i =0; i < theParticles->size(); i++)
+    {
+        const art::Ptr<recob::PFParticle> particle(theParticles, i);
+        const int pdg(particle->PdgCode());
+        if (pdg == 13)
+        {
+            m_nTracks++;
+        }
+        else if (pdg == 11)
+        {
+            m_nShowers++;
+        }
+        else
+        {
+            std::cout << "Found PFParticle with PDG code: " << pdg << std::endl;
+        }
+    }
+
+    m_tree->Fill();
 }
 
 void test::MyTestAnalyzer::reconfigure(fhicl::ParameterSet const & p)
 {
   // Implementation of optional member function here.
   //here you add an external fcl file to change configuration
+    m_particleLabel = p.get<std::string>("PFParticleModule", "pandoraNu");
 }
 
 DEFINE_ART_MODULE(test::MyTestAnalyzer)
